c11ca55ca5d6b47f6e66625ab82bb4d2f8dad4ab
Miguel <migueldelalibera9@gmail.com>
2024-11-03 11:38:40 -0300

    Adicionado validacao de email e senha

--- script/login.js
+++ script/login.js
@@ -6,6 +6,17 @@ function login() {
     if (camposEstaoVazios()) {
         return
     }
+
+    const usuarioDigitado = {
+        email: email.value,
+        senha: senha.value        
+    }
+
+    if (validarLogin(usuarioDigitado) == null) {
+        alert('Email e/ou senha invalidos')
+    } else {
+        //fazer login
+    }
 }
 
 function camposEstaoVazios() {
@@ -15,4 +26,23 @@ function camposEstaoVazios() {
     }
 
     return false
+}
+
+
+function validarLogin(usuario) {
+    let usuarioValidado = null
+
+    for (const chave in localStorage) {
+        const objeto = JSON.parse(localStorage.getItem(chave))
+
+        if (objeto && objeto.hasOwnProperty('CPF')) { // Se o objeto existe e tem a propriedade CPF significa que é um usuario então posso fazer as validações
+            if (objeto.email == usuario.email && objeto.senha == usuario.senha) {
+                usuarioValidado = objeto
+            }
+        }
+    }
+
+    return usuarioValidado
+
+    
 }
