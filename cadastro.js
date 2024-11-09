6bec96a85bd364fee330b71bd7c735389b173fba
Abraão <abraao.ar05@gmailcom>
2024-11-09 09:37:56 -0300

    cadastro.js alterado

--- cadastro.js
+++ cadastro.js
@@ -9,14 +9,3 @@ function mostrar_cnpj(){
     document.getElementById('label_cp').style.display = 'block';
     document.getElementById('input_cp').setAttribute('placeholder', "CNPJ")
 }
-
-//Formata o input date
-function formatar_dara(){
-    let data_input = document.getElementById('data')
-    let data = data_input.value()
-
-    if(data){
-        const [year, month, day] = dateValue.split("-");
-        data.value = `${day}/${month}/${year}`; 
-    }
-}
