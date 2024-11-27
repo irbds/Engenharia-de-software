1b6d1da5a372b38893fa59931324b6c6bc2ae3ae
Miguel <migueldelalibera9@gmail.com>
2024-11-27 20:05:01 -0300

    Adicionada geração dinamica do html dos cards de doacao

--- script/mostraDoacoes.js
+++ script/mostraDoacoes.js
@@ -1,9 +1,23 @@
 function mostrarDoacoes() {
     let doacoes = getDoacoes()
 
+    const container = document.getElementById('doacoes-container')
+    container.innerHTML = ''
+
+    doacoes.forEach((doacao) => {
+        const card = `
+        <div class="card-doacao">
+            <h4>${doacao.nome_doacao}</h4>
+            <p>${doacao.Descricao}</p>
+            <p><span>Doado: ${doacao.valor_doado}</span>/<span>${doacao.valor}</span></p>
+            <p><span>Até: ${doacao.data_final}</span></p>
+            <button value="Doar" class="Doar" id="doar"></button>
+        </div>`
+        
+        container.innerHTML += card
+    })
     
 }
-
 
 
 
